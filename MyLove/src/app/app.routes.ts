7803f53139fe459b61d6a1b467b97559c241f602
samanazadi1996 <samanazadi1996@gmail.com>
2026-09-10 20:43:58 +0330

    Publish in 2026-09-10-20-43

--- MyLove/src/app/app.routes.ts
+++ MyLove/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'Page1', component: HomeComponent },
   { path: 'love/:id', component: QuestionnaireComponent },
+  { path: 'Page1/love/:id', component: QuestionnaireComponent },
   { path: '**', component: NotFoundComponent }
 ];
